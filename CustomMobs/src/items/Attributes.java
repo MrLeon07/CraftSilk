fef213c474458276c832635b0cb2c3d7422b35ad
Leon <Leon@DESKTOP-881MNNM>
2021-02-26 09:48:52 +0300

    a

--- CustomMobs/src/items/Attributes.java
+++ CustomMobs/src/items/Attributes.java
@@ -23,16 +23,16 @@ public class Attributes {
 	NBTTagList modifiers = new NBTTagList();
 	NBTTagCompound damage = new NBTTagCompound();
 	NBTTagCompound attackSpeed = new NBTTagCompound();
-	damage.setString("AttributeName", "generic.attackDamage");
-	damage.setString("Name", "generic.attackDamage");
+	damage.setString("AttributeName", "generic.attack_damage");
+	damage.setString("Name", "generic.attack_damage");
 	damage.setString("Slot", "mainhand");
 	damage.setInt("Operation", 0);
 	damage.setDouble("Amount", rand.nextDouble()*100);
 	damage.setInt("UUIDMost", rand.nextInt(32000)+1);
 	damage.setInt("UUIDLeast", rand.nextInt(64000)+32001);
 	
-	attackSpeed.setString("AttributeName", "generic.attackSpeed");
-	attackSpeed.setString("Name", "generic.attackSpeed");
+	attackSpeed.setString("AttributeName", "generic.attack_speed");
+	attackSpeed.setString("Name", "generic.attack_speed");
 	attackSpeed.setString("Slot", "mainhand");
 	attackSpeed.setInt("Operation", 0);
 	attackSpeed.setDouble("Amount", -3.50);
